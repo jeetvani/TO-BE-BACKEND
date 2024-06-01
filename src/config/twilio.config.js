7dd94dfd1058c1f08557a798cfc17abb2f402ee1
jeetvani <210306105030@paruluniversity.ac.in>
2024-06-02 03:15:34 +0530

    chore: Update Twilio credentials in twilio.config.js

--- src/config/twilio.config.js
+++ src/config/twilio.config.js
@@ -1,6 +1,6 @@
 const twilioProdConfig = {
-    accountSid: 'ACc95dd4d2c9fde917389558818b899607',
-    authToken: '7d3e809f56b51eecfa297c34a55d99a2',
+    accountSid: 'AC6a79e8a4343dc47b39e2556f0cccd353',
+    authToken: 'bed2141537fc4bd1f2a0919fc28807d2',
     fromPhoneNumber: '+18189460736',
 }
 
